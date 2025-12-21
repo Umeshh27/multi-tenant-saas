68cb4deee5a3ea29454584a0d6ad6a994043c181
Umeshh27 <23MH1A05E7@acoe.edu.in>
2025-12-21 18:01:28 +0530

    style: add global CSS styles for consistent layout and design

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import api from "../api/api";
 import { useNavigate, Link } from "react-router-dom";
+import '../App.css';
 
 function Login() {
   const navigate = useNavigate();
